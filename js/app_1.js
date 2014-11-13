dd8ae2c48128bcd5ceddb1d3f5e65e10e97342c2
Alex Ubben <alexandra.a.ubben@usace.army.mil>
2014-11-13 13:21:34 -0600

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var mapId = 'aubben.k7c22gcp'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1IjoiYXViYmVuIiwiYSI6InNQM25NSzQifQ.WJJjBKBKFusEPUY-eFayYQ'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
